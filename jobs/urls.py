7962f03499a865af1e8f5bc4bfd908c61c982b60
ranju <sharmaranju684@gmail.com>
2026-05-09 15:27:39 +0545

    Added Dashboard Endpoint

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,7 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import JobApplicationViewset
+from .views import JobApplicationViewset, DashboardView
+from django.urls import path
 
 router = DefaultRouter() # Instantiates a router that automatically creates URL patterns for your API. 
 router.register(r'jobs', JobApplicationViewset, basename='job')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('dashboard/', DashboardView.as_view()),
+]
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,11 @@
 from rest_framework import viewsets, permissions
+from rest_framework.views import APIView
+from django.db.models import Count
+from django.utils import timezone
+from rest_framework.response import Response
 from .models import JobApplication
 from .serializers import JobApplicationSerializer
+
 
 # Create your views here.
 class JobApplicationViewset(viewsets.ModelViewSet):
@@ -21,3 +26,37 @@ class JobApplicationViewset(viewsets.ModelViewSet):
         # add user_id when a new application is added
         serializer.save(user=self.request.user)
 
+
+class DashboardView(APIView):
+    def get(self, request):
+        user = request.user
+        applications = JobApplication.objects.filter(user=user)
+        total = applications.count()
+
+        by_status = {}
+        status_counts = applications.values('status').annotate(count=Count('id'))
+        ''' it returns sth like this => 
+            [
+                {'status': 'applied', 'count': 8},
+                {'status': 'interviewing', 'count': 2},
+            ]'''
+        for item in status_counts:
+            by_status[item['status']] = item['count']  # 'applied': 8
+
+        # upcoming interviews
+        upcoming_interviews = applications.filter(
+            interview_date__gte = timezone.now().date()).order_by('interview_date')
+        
+
+        # stale applications with no update in 30 days
+        thirty_days_ago = timezone.now() - timezone.timedelta(days=30)
+        stale = applications.filter(
+            updated_at__lte = thirty_days_ago
+        )
+
+        return Response({
+            'total': total,
+            'by_status': by_status,
+            'upcoming_interviews': JobApplicationSerializer(upcoming_interviews, many=True).data,
+            'stale_applications': JobApplicationSerializer(stale, many=True).data,
+        })
