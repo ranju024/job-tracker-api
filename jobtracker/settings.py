77d608442dff68b6c2c3fa31c9975b718add58ee
ranju <sharmaranju684@gmail.com>
2026-05-09 20:48:24 +0545

    added CORS for react frontend

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     "jobs",
     "accounts",
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -76,6 +77,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
 ]
 
 ROOT_URLCONF = "jobtracker.urls"
