fd5b59d41b8b3590de71670fc156ae1c38d252c6
Ranju Sharma <sharmaranju684@gmail.com>
2026-08-20 17:50:33 +0545

    env variables added

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 AUTH_USER_MODEL = 'accounts.User'
 
 # Application definition
@@ -124,12 +124,15 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-]
+CORS_ALLOWED_ORIGINS = env.list(
+    'CORS_ALLOWED_ORIGINS',
+    default=[
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+    ],
+)
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = DEBUG  # only True in local development
 ROOT_URLCONF = "jobtracker.urls"
 
 TEMPLATES = [
